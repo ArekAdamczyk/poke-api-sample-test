cf40c2df4dd0f8a219a2791ef342094c82a12263
ArekAdamczyk <94310930+ArekAdamczyk@users.noreply.github.com>
2023-07-03 15:29:00 +0200

    fix testInit.js

--- test/init/testInit.js
+++ test/init/testInit.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const chai = require('chai')
-  ; const chaiHttp = require('chai-http');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 
 function initTests() {
   chai.use(chaiHttp);
+  chai.use(require('chai-json-schema-ajv'));
   global.expect = chai.expect;
   global.request = chai.request;
 };
