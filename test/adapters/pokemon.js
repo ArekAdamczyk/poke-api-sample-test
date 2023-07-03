ea4caf3eb2943a692819df01bb6530de906185e7
ArkadiuszAdamczyk <arkadiusz.adamczyk@intergiro.com>
2023-07-03 12:07:31 +0200

    add sample tests

---
+++ test/adapters/pokemon.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const chai = require('chai') 
+
+async function getPokemonList(queryLimit = 20) {
+    return request(`${process.env.NODE_ENV}/api/v2/pokemon/`)
+        .get('/')
+        .query({limit: queryLimit})
+}
+
+async function getPokemon(id) {
+    return request(`${process.env.NODE_ENV}/api/v2/pokemon/${id}/`)
+        .get('/');
+}
+
+module.exports = {
+    getPokemonList,
+    getPokemon,
+}
